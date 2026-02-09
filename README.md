a61e96129f586a0740cd6f9164541583664f30b0
Gabriel Sá <gabrielsns2016@gmail.com>
2026-02-09 10:05:10 -0300

    Testes finais e correção no tipo de model do "content" e no README

--- README.md
+++ README.md
@@ -80,3 +80,6 @@ The API will be available at: http://127.0.0.1:8000/
     - views.py: API logic (ViewSet).
 
     - urls.py: App-specific routes.
+
+
+Made by Gabriel Sá
--- api_rest/models.py
+++ api_rest/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     username = models.CharField(max_length=100, default='')
     title = models.CharField(max_length=100, default='')
-    content = models.CharField( default='')
+    content = models.TextField( default='')
     created_at = models.DateTimeField(auto_now_add=True) 
     updated_at = models.DateTimeField(auto_now=True) 
 
