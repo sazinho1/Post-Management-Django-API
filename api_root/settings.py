73c3311501adc6f45632dad7fc5c0b92126cb9d6
Gabriel Sá <gabrielsns2016@gmail.com>
2026-02-08 21:50:03 -0300

    Colocando a paginação automatica

--- api_root/settings.py
+++ api_root/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 CORS_ALLOW_ORIGINS = [
     'http://localhost:8080'
 ]
+
+# Automatic pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
